0615f455f719b8f260853b9391e5c64b3ad3aa96
Aman Bhandari <asbhandari012@gmail.com>
2026-08-12 23:02:27 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ 
+public class Main{
+    
+    public static void main(String args[]){
+        Scanner ui = new Scanner(System.in);
+        
+        int m = ui.nextInt(); 
+        int n = ui.nextInt();
+        
+        System.out.println((m * n) / 2);
+    }
+}
